7725522651ecd20e3d0dcf0ca28367b46d0965b7
Ilyas Ovayurt <iovayurt@gmail.com>
2022-05-10 18:49:54 +0300

    collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,5 +29,21 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 }
 
